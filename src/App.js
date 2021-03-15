0ebeed16f0cab33d52a64ada458d3d8d078cd2e6
grungebarb <barbarallanso@gmail.com>
2021-03-15 21:04:22 +0100

    Created and linked the first component and its css document

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 
 import './App.css';
+import Weather from './Weather';
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-        Weather App
+        <Weather />
         <footer>
           This project was coded by GrungeBarb (Bárbara Llansó) and is{" "}
           <a href="https://github.com/grungebarb/react-weather-app" target="_blank" rel="noreferrer">
