c530716ab92e42825818a697ef6fa98e6df7ba12
grungebarb <barbarallanso@gmail.com>
2021-03-15 20:41:17 +0100

    Added the GitHub repository URL

--- src/App.js
+++ src/App.js
@@ -5,6 +5,12 @@ export default function App() {
   return (
     <div className="App">
         Weather App
+        <footer>
+          This project was coded by GrungeBarb (Bárbara Llansó) and is{" "}
+          <a href="https://github.com/grungebarb/react-weather-app" target="_blank" rel="noreferrer">
+            open-sourced on GitHub
+          </a>
+        </footer>
     </div>
   );
 }
